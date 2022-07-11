f3e3bd6a23b32e12de83ffa06ceef4c14893c6c5
Felipe Santos <fe.campossantos@gmail.com>
2022-07-10 22:03:12 -0300

    changeee some stuff on test

--- app/multithread_test/multithread_test.cc
+++ app/multithread_test/multithread_test.cc
@@ -10,36 +10,36 @@
 
 using namespace EPOS;
 
-#define THREAD_COUNT 8
+#define N_THREADS 4
 
-Thread * workers[8];
+Thread * thread_list[8];
 
 OStream cout;
 
-int worker(int i);
+int calculate(int i);
 
 int main()
 {
-    cout << "Starting scheduling tests with " << CPU::cores() << " cores" << endl;
+    cout << "Running multithread test with " << CPU::cores() << " cores" << endl;
     cout << endl;
 
-    for (auto i = 0; i < THREAD_COUNT; i++) {
-        workers[i] = new Thread(&worker, i);
-        cout << "Created thread " << i << endl;
+    for (auto i = 0; i < N_THREADS; i++) {
+        cout << "Creating thread #" << i << endl;
+        thread_list[i] = new Thread(&calculate);
     }
 
     // Each time one of the words is swapped from a queue to another
     // a WRN is raised, we kept warnings ON for test purposes
-    for (auto i = 0; i < THREAD_COUNT; i++) {
-        int work = workers[i]->join();
-        cout << "Thread " << i << " returned from core " << work << " with priority " << workers[i]->criterion() << endl;
+    for (auto i = 0; i < N_THREADS; i++) {
+        int core = thread_list[i]->join();
+        cout << "Thread " << i << " returned from core " << core << " with priority " << thread_list[i]->criterion() << endl;
     }
 
     return 0;
 }
 
 // Worker with dummy operations that take long enough to be preempted
-int worker(int i) {
+int calculate() {
     int j = 0;
 
     int core = CPU::id();
