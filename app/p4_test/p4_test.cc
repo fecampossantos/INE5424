26686070d4edbe9de5bb09616a0c24c9092be3c1
Pablo Strapasson <pablostrapasson98@gmail.com>
2022-07-20 00:24:52 -0300

    adicionado explicação dos testes

--- app/p4_test/p4_test.cc
+++ app/p4_test/p4_test.cc
@@ -16,34 +16,32 @@ int IO_bound();
 
 int main()
 {
-  // cout << "PMS Scheduler test" << endl;
-
-  // cout << "Creating CPU threads" << endl;
   thread[0] = new Thread(&CPU_bound);
   thread[1] = new Thread(&CPU_bound);
   thread[2] = new Thread(&CPU_bound);
 
-  // cout << "Creating IO threads" << endl;
   thread[3] = new Thread(&IO_bound);
   thread[4] = new Thread(&IO_bound);
   thread[5] = new Thread(&IO_bound);
 
-  // cout << "Threads created!" << endl;
-
-  
-  // sugiro adicionar ao final uma impressão dos parâmetros que
-  // seriam utilizados para classificar a thread, para garantir que se comportam como esperado.
+  // Nesse teste criamos 6 threads, e por padrão todas são criadas como CPU_BOUND (escolha de projeto)
+  // Nosso teste se baseia em verificar a alteração do tipo da thread, em específico as três últimas, 
+  // para IO_BOUND. Essa mudança ocorre quando a thread entra no estado waiting (sleep()).
+  // Para a verificação imprimimos para as três primeiras threads um "C" indicando CPU_BOUND acompanhado do tipo
+  // com o qual ela retornou, nesse caso esperamos tipo '1'. A confirmação do tipo é feita no segundo 'cout'
+  // onde comparamos o tipo recebido com o esperado.Nesse caso quando o retorno é 1 indica True e a 
+  // verificação é verdadeira. O mesmo para as últimas três threads, com a alteração da impressão de um "I"
+  // indicando IO_BOUND e o tipo esperado é '0'.
+  // Esses números (0, 1) vem no ENUM onde definimos os dois tipos (IO_BOUND, CPU_BOUND) em process.h.
   for (int i = 0; i < 6; i++)
   {
     thread[i]->join();
 
     if (i < 3){
-      // cout << "CPU Bound Thread " << thread[i] << " finished with type: " << thread[i]->type() << endl;
       cout << "C" << thread[i]->type() << endl;
       cout << (thread[i]->type() == 1) << endl; // should return 1
     }
     else {
-      // cout << "IO Bound Thread " << thread[i] << " finished with type: " << thread[i]->type() << endl;
       cout << "I" << thread[i]->type() << endl;
       cout << (thread[i]->type() == 0) << endl; // should return 0
     }
@@ -59,7 +57,6 @@ int main()
 
 int CPU_bound()
 {
-  // cout << "CPU bound process... " << endl;
 
   int n1 = 0, n2 = 1, result;
   for (size_t i = 0; i < 3000; i++)
@@ -74,7 +71,6 @@ int CPU_bound()
     }
   }
 
-  // cout << "Fibonacci of " << 10 << " is " << result << endl;
   return 0;
 }
 
@@ -82,11 +78,9 @@ int IO_bound()
 {
   for (size_t i = 0; i < 4; i++)
   {
-    // cout << "IO bound process -> Alarm::delay()" << endl;
 
     Alarm::delay(10000);
 
-    // cout << "IO bound process | Done waiting. " << endl;
   }
 
   return 0;
